4c8c486e44c121ab80b6ebf132010d682f77ad06
Zhou Bin <bin.zhou@ham-let.com>
2019-04-04 15:12:42 +0800

    ref: add wrong code
    test email
    
    Change-Id: I8485d9df640b8b5ed519e4693f506850e50551b9

--- main.c
+++ main.c
@@ -7,11 +7,15 @@
 
 int main(void)
 {
-	//add comment again
-	//add comment 2
-	//add comment 3
-	//add comment 4
-	int i = 0;
-	int j = 1;
-	return 0;
+    //add comment again
+    //add comment 2
+    //add comment 3
+    //add comment 4
+    int i = 0;
+    int j = 1;
+    int k = 3;
+    int l = 3;
+    int m = 3;
+    int n = 3;
+    return 0;
 }
