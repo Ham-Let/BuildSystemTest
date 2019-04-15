d667df24b13c6b6a2efc5ba81891892660b518df
Zhou Bin <bin.zhou@ham-let.com>
2019-04-15 17:21:24 +0800

    ref: tt
    Change-Id: Idabf55fbebe3e83a6728b53be4958a08b09eee34

--- main.c
+++ main.c
@@ -7,11 +7,9 @@
 
 int main(void)
 {
-	//add comment again
-	//add comment 2
-	//add comment 3
-	//add comment 4
-	int i = 0;
-	int j = 1;
-	return 0;
+    //add comment again
+    //add comment 2
+    //add comment 3
+    //add comment 4
+    return 0;
 }
